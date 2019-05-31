f1897910eaa36dafeb1b6285781ab83a2cd6497b
Saul Avila <saul_harry_@hotmail.com>
2019-05-31 17:57:48 -0500

    se crea el servicio que usara peticiones http para usar el backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes:Routes = [
   {path:'', redirectTo:"/clientes", pathMatch:'full'},
@@ -25,7 +26,8 @@ const routes:Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [ClienteService],
   bootstrap: [AppComponent]
--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -6,7 +6,9 @@ import {Cliente} from './cliente';
 
 /* Esto es para angular 6*/
 /*El Observable sirve para realizar las peticiones asincronas y poder usar Reactive*/
-import { Observable, of } from 'rxjs'
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
@@ -14,10 +16,26 @@ import { Observable, of } from 'rxjs'
 })
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint:string = 'http://localhost:8080/api/clientes';
+
+  constructor(private http:HttpClient) { }
 
   getClientes():Observable<Cliente[]>{
-    /*Con of() se convierte el listado de clientes en un flujo observable apartir de la lista de clientes*/
-    return of(CLIENTES);
+    /*Con of() se convierte el listado de
+     clientes en un flujo observable apartir
+     de la lista de clientes*/
+    //return of(CLIENTES);
+
+    /*Esto se hace para realizar un cast ya que por
+     defecto el metodo .get devuelve un objeto de
+      tipo any y necesitamos un arreglo de clientes*/
+      return this.http.get<Cliente[]>(this.urlEndPoint);
+
+    /*
+    esto es una segunda forma, funciona igual solo que con otra sintaxis
+    */
+  /*  return this.http.get(this.urlEndPoint).pipe(
+      map(response => response as Cliente[])
+    );*/
   }
 }
