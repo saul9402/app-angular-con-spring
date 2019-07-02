470679a6965b0ae4699797a9ebe6fe473a9b58cf
Saul Avila <saul_harry_@hotmail.com>
2019-07-02 11:10:33 -0500

    se agrega metodo login para implentar en proximas clases, asi como la clase usuario que manejara los datos de inicio de sesión

--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from './usuario';
+
+import  swal  from "sweetalert2";
 
 @Component({
   selector: 'app-login',
@@ -6,11 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  titulo:string = "Pro favor Sign In!";
+  titulo: string = "Por favor Sign In!";
+  usuario: Usuario;
 
-  constructor() { }
+  constructor() {
+    this.usuario = new Usuario();
+  }
 
   ngOnInit() {
+  }
+
+  login(): void {
+    console.log(this.usuario);
+    if(this.usuario.username == null || this.usuario.username == "" || this.usuario.password == null || this.usuario.password == ""){
+      swal.fire("Error Login", "Username o password vacíos!", 'error');
+      return;
+    }
   }
 
 }
