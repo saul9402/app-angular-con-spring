7607b942cbdb70aed6e4f7859f39840a3eacc0a4
Saul Avila <saul_harry_@hotmail.com>
2019-07-02 16:56:00 -0500

    se agrega guard con roles y tambien se valida en el auth guard que el token no haya expirado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DetalleComponent } from './clientes/detalle/detalle.component';
 import { LoginComponent } from './usuarios/login.component';
 import { AuthGuard } from './usuarios/guards/auth.guard';
+import { RoleGuard } from './usuarios/guards/role.guard';
 
 
 registerLocaleData(localeES, 'es');
@@ -31,8 +32,8 @@ const routes: Routes = [
   { path: 'directivas', component: DirectivaComponent },
   { path: 'clientes', component: ClientesComponent },
   { path: 'clientes/page/:page', component: ClientesComponent },
-  { path: 'clientes/form', component: FormComponent, canActivate: [AuthGuard] },
-  { path: 'clientes/form/:id', component: FormComponent, canActivate: [AuthGuard] },
+  { path: 'clientes/form', component: FormComponent, canActivate: [AuthGuard, RoleGuard], data: { role: 'ROLE_ADMIN' } },
+  { path: 'clientes/form/:id', component: FormComponent, canActivate: [AuthGuard, RoleGuard], data: { role: 'ROLE_ADMIN' } },
   { path: 'login', component: LoginComponent }
 ]
 
--- src/app/usuarios/guards/auth.guard.ts
+++ src/app/usuarios/guards/auth.guard.ts
@@ -16,10 +16,27 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
     if (this.authService.isAuthenticated()) {
+      if (this.isTokenExpirado) {
+        this.authService.logout();
+        swal.fire("Token Expirado", `Hola ${this.authService.usuario.username} tu sesión ha expirado. Inicia sesión de nuevo`, 'info');
+        this.router.navigate(['/login']);
+        return false;
+      }
       return true;
     }
     swal.fire("Usuario Ánonimo", "Debes autenticarte para acceder a este recurso", 'warning');
     this.router.navigate(['/login']);
+    return false;
+  }
+
+  isTokenExpirado(): boolean {
+    let token = this.authService.token;
+    let payload = this.authService.obtenerDatosToken(token);
+    // Obtiene fecha actual en segundos
+    let now = new Date().getTime() / 1000;
+    if (payload.exp < now) {
+      return true;
+    }
     return false;
   }
 
