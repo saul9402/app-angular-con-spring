287f748dd0a3903aa0bad65b07f98af1467f987e
Saul Avila <saul_harry_@hotmail.com>
2019-07-02 14:53:27 -0500

    se agrega mensaje de adveretencia cuando el usuario intenta acceder a un recurso que no le corresponde

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -38,8 +38,13 @@ export class ClienteService {
   }
 
   private isNoAutorizado(e): boolean {
-    if (e.status == 401 || e.status == 403) {
-      this.router.navigate(['/login'])
+    if (e.status == 401) {
+      this.router.navigate(['/login']);
+      return true;
+    }
+    if (e.status == 403) {
+      swal.fire("Acceso Denegado", `Hola ${this.authService.usuario.username} no tienes accesos a este recurso`, 'warning');
+      this.router.navigate(['/clientes']);
       return true;
     }
     return false;
