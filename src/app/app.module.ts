6dabff67a9c84c785dd18fd22b7f02801667aa35
Saul Avila <saul_harry_@hotmail.com>
2019-07-02 15:29:11 -0500

    se agrega "guard" que permite controlar mejor el acceso a las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
-import {  registerLocaleData } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -22,17 +22,18 @@ import localeES from '@angular/common/locales/es';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DetalleComponent } from './clientes/detalle/detalle.component';
 import { LoginComponent } from './usuarios/login.component';
+import { AuthGuard } from './usuarios/guards/auth.guard';
 
 
 registerLocaleData(localeES, 'es');
-const routes:Routes = [
-  {path:'', redirectTo:"/clientes", pathMatch:'full'},
-  {path:'directivas', component: DirectivaComponent},
-  {path:'clientes', component: ClientesComponent},
-  {path:'clientes/page/:page', component: ClientesComponent},
-  {path:'clientes/form', component: FormComponent},
-  {path:'clientes/form/:id', component: FormComponent},
-  {path:'login', component: LoginComponent}
+const routes: Routes = [
+  { path: '', redirectTo: "/clientes", pathMatch: 'full' },
+  { path: 'directivas', component: DirectivaComponent },
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'clientes/page/:page', component: ClientesComponent },
+  { path: 'clientes/form', component: FormComponent, canActivate: [AuthGuard] },
+  { path: 'clientes/form/:id', component: FormComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent }
 ]
 
 @NgModule({
@@ -57,7 +58,7 @@ const routes:Routes = [
     MatMomentDateModule,
     BrowserAnimationsModule
   ],
-  providers: [ClienteService, {provide: LOCALE_ID, useValue: 'es' }],
+  providers: [ClienteService, { provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/usuarios/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth.service';
+import swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {
+
+  }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    if (this.authService.isAuthenticated()) {
+      return true;
+    }
+    swal.fire("Usuario Ánonimo", "Debes autenticarte para acceder a este recurso", 'warning');
+    this.router.navigate(['/login']);
+    return false;
+  }
+
+}
