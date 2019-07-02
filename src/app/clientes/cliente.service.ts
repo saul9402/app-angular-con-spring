675cb6ee6070bf3d49d966b4e569e0b2e96de9c8
Saul Avila <saul_harry_@hotmail.com>
2019-07-02 15:16:32 -0500

    se agrega validación para cuando el token haya expirado

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -39,6 +39,9 @@ export class ClienteService {
 
   private isNoAutorizado(e): boolean {
     if (e.status == 401) {
+      if (this.authService.isAuthenticated()) {
+        this.authService.logout();
+      }
       this.router.navigate(['/login']);
       return true;
     }
