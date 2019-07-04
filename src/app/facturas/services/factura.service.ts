fa9000c9d238ed286c00d272766baa7d38f28957
Saul Avila <saul_harry_@hotmail.com>
2019-07-04 11:51:36 -0500

    se agrega servicio y vista para ver el detalle de la factura de cada cliente

---
+++ src/app/facturas/services/factura.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Factura } from '../models/factura';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FacturaService {
+
+  private urlEndPoint: string = 'http://localhost:8080/api/facturas';
+
+  constructor(private http: HttpClient) { }
+
+  getFactura(id: number): Observable<Factura> {
+    console.log("el end point es:" + this.urlEndPoint);
+    return this.http.get<Factura>(`${this.urlEndPoint}/${id}`);
+  }
+}
