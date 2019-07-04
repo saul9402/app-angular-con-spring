fa146d5d04bf40ab098d30316d10794b4ad81426
Saul Avila <saul_harry_@hotmail.com>
2019-07-04 10:01:41 -0500

    se agregan clases para manejar los datos que llegaran desde el backend

--- src/app/clientes/cliente.ts
+++ src/app/clientes/cliente.ts
@@ -1,12 +1,15 @@
 import { Region } from './Region';
+import { Factura } from '../facturas/models/factura';
 
 export class Cliente {
 
-  id:number;
+  id: number;
   nombre: string;
-  apellido:string;
-  createAt:string;
-  email:string;
-  foto:string;
+  apellido: string;
+  createAt: string;
+  email: string;
+  foto: string;
   region: Region;
+  // facturas: Factura[] = [];
+  facturas: Array<Factura> = [];
 }
---
+++ src/app/facturas/models/item-factura.ts
@@ -0,0 +1,9 @@
+import { Producto } from './producto';
+
+export class ItemFactura {
+
+    producto: Producto;
+    cantidad: number = 1;
+    importe: number;
+
+}
